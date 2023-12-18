a5cd4dc0db8f9280989d495ac0a2715359e5625b
rickrosser <richard.d.rosser@gmail.com>
2023-12-18 14:28:31 -0500

    Initial Project Creation

---
+++ HelloWorldiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldiOSApp.swift
+//  HelloWorldiOS
+//
+//  Created by Rick Rosser on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
